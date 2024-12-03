aec84250c1aa068e2a91ab01d154329d7445e00c
ifgiahmad <ifgi.ahmad@gmail.com>
2024-12-03 11:46:35 +0700

    Update upload-photo-form.tsx

--- components/form/upload-photo-form.tsx
+++ components/form/upload-photo-form.tsx
@@ -260,7 +260,6 @@ const UploadPhotoForm: React.FC<UploadPhotoFormProps> = ({
                     <input
                       type="file"
                       accept="image/*"
-                      capture="environment"
                       onChange={handleFileChange}
                       className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
                     />
